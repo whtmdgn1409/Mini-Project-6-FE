ff46b520e04de51c089323ad7c5f52603063256d
quokka-eating-carrots <ovzip7@gmail.com>
2023-02-13 21:56:33 +0900

    [#5] Feat: 마이 페이지 마크업 & 메뉴바 컴포넌트

--- src/components/UserInfo/LikeBox.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LikeBox = () => {
-  return <div>LikeBox</div>;
-};
-
-export default LikeBox;
--- src/pages/UserInfoPage/index.tsx
+++ src/pages/UserInfoPage/index.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import LikeBox from '../../components/UserInfo/LikeBox';
-import LoanBox from '../../components/UserInfo/LoanBox';
 
 type Props = {};
 
 const UserInfoPage = (props: Props) => {
-  return (
-    <div>
-      <h1 className='text-[20px]'>
-        <span className='text-mw font-bold'>미왕이</span> 님 안녕하세요!
-      </h1>
-      <span className='text-[14px]'>이용 중인 대출 3건</span>
-      <div className='flex gap-5 flex-col items-center'>
-        <LoanBox />
-        <LoanBox />
-        <button className='mwBtn !h-[30px] !text-[14px]'>더 보기</button>
-      </div>
-      <br />
-      <h2 className='text-[20px] font-bold'>관심 상품</h2>
-      <LikeBox />
-    </div>
-  );
+  return <div>UserInfoPage</div>;
 };
 
 export default UserInfoPage;
