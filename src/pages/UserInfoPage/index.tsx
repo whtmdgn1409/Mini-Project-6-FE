f09fa33b496541a2f40e77f6125176d8fc60583f
quokka-eating-carrots <ovzip7@gmail.com>
2023-02-13 22:00:05 +0900

    [#5] Feat: 유저 정보 수정 페이지 마크업

--- src/pages/UserInfoPage/index.tsx
+++ src/pages/UserInfoPage/index.tsx
@@ -3,7 +3,18 @@ import React from 'react';
 type Props = {};
 
 const UserInfoPage = (props: Props) => {
-  return <div>UserInfoPage</div>;
+  return (
+    <div>
+      <div>
+        <h2>비밀번호 변경</h2>
+        <h2>맞춤 정보 변경</h2>
+      </div>
+      <div>
+        <input type='text' placeholder='기존 비밀번호를 입력해 주세요.' />
+        <input type='text' placeholder='새로운 비밀번호를 입력해 주세요.' />
+      </div>
+    </div>
+  );
 };
 
 export default UserInfoPage;
