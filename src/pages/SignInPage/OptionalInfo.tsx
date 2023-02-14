988b8fa0d8f8dbd49f6e32f4f3ce902bc829255b
hyorimm <hyorimm@gmail.com>
2023-02-15 08:44:29 +0900

    [#8] Feat: 문구 수정, 버튼 수정, 로그인 페이지로 연결

--- src/pages/SignInPage/OptionalInfo.tsx
+++ src/pages/SignInPage/OptionalInfo.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import InfoSelect from '../../components/InfoSelect';
 
 type Props = {};
@@ -39,13 +40,17 @@ const banks = [
 ];
 
 const OptionalInfo = (props: Props) => {
+  const navigate = useNavigate();
+
   return (
     <section className='w-[300px] m-auto'>
       <h1 className='text-4xl font-bold text-center'>궁금해요</h1>
       <p className='text-center mt-4 mw-lGray'>
+        미왕론의 회원이 되신 것을 축하해요!🎉
+        <br />
         아래 정보를 추가 입력해주시면
         <br />
-        맞춤 상품을 추천해드릴게요☺
+        맞춤 상품도 추천해드릴게요☺
       </p>
       <form className='flex-col mt-8 text-center'>
         <InfoSelect infos={jobs} />
@@ -58,8 +63,13 @@ const OptionalInfo = (props: Props) => {
         <button className='mwBtn font-semibold mt-12 !w-[140px]'>
           제출하기
         </button>
-        <button className='mwBtn shadow font-semibold mt-12 !w-[140px]'>
-          다음에 하기
+        <button
+          onClick={() => {
+            navigate('/signup');
+          }}
+          className='mwBtn-white shadow-default font-semibold mt-12 !w-[140px]'
+        >
+          다음에 할게요
         </button>
       </div>
     </section>
