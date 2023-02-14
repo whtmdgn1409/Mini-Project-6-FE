b9396c9434556e035556cd989bff6569c78d83f9
quokka-eating-carrots <ovzip7@gmail.com>
2023-02-14 17:06:36 +0900

    [#5] Feat: 마이 페이지 마크업 & 헤더 CSS 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,17 +5,17 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <div className='flex relative m-auto top-8 h-12 w-96 '>
-      <div className='absolute inset-x-0 top-0 text-center'>
+    <div className='flex relative m-auto h-[100px] w-96 items-center justify-center'>
+      <div className='inset-x-0 top-0 text-center'>
         <span className='text-mw font-semibold'>MW </span>
         <span className='--black-100 font-light'>Bank</span>
+        <button className='absolute right-2 top-[50%] translate-y-[-50%]'>
+          <Link to='/'>
+            {' '}
+            <AiOutlineMenu />
+          </Link>
+        </button>
       </div>
-      <button className='absolute right-2 top-1'>
-        <Link to='/'>
-          {' '}
-          <AiOutlineMenu />
-        </Link>
-      </button>
     </div>
   );
 };
