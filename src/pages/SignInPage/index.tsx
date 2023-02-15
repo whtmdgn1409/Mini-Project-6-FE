4c02610ff08a22e8f5244b11e978010ec9a58172
hyorimm <hyorimm@gmail.com>
2023-02-15 13:53:02 +0900

    [#8] Feat: 회원가입 유효성 검사

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -1,9 +1,48 @@
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 type Props = {};
 
+interface IvalidationForm {
+  name: string;
+  password: string;
+  email: string;
+  mobile: string;
+}
+
 const SignInPage = (props: Props) => {
   const navigate = useNavigate();
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .max(5, '최대 길이는 5글자 입니다')
+      .required('이름을 입력해주세요'),
+    password: yup
+      .string()
+      .min(8, '8자 이상 16자 이하의 숫자 혹은 문자를 입력해주세요')
+      .max(16, '8자 이상 16자 이하의 숫자 혹은 문자를 입력해주세요')
+      .required('비밀번호를 입력해주세요'),
+    email: yup
+      .string()
+      .email('이메일 형식에 맞추어 입력해주세요')
+      .required('이메일 형식에 맞추어 입력해주세요'),
+    mobile: yup
+      .string()
+      .matches(/^[0-9]{11}$/i, '번호는 01012345678형태로 입력해주세요')
+      .required('휴대폰 번호를 입력해주세요'),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IvalidationForm>({
+    resolver: yupResolver(schema),
+  });
+  const check = (data: string) => {
+    console.log(data);
+  };
   return (
     <section className='w-[300px] m-auto'>
       <h1 className='text-4xl font-bold text-center'>반가워요</h1>
@@ -12,14 +51,23 @@ const SignInPage = (props: Props) => {
         <br />
         금방 끝나요!😉
       </p>
-      <form className='flex-col mt-16 text-center'>
+      <form
+        onSubmit={handleSubmit((data) => console.log(data))}
+        className='flex-col mt-16 text-center'
+      >
         <div>
           <label htmlFor='name'></label>
           <input
             type='text'
             className='mwInput pl-6 w-full'
             placeholder='이름'
+            {...register('name')}
           />
+          {errors.name && (
+            <p className='text-sm text-alert font-semibold pt-3'>
+              {errors.name.message}
+            </p>
+          )}
         </div>
         <div className='mt-8'>
           <label htmlFor='password'></label>
@@ -27,15 +75,27 @@ const SignInPage = (props: Props) => {
             type='password'
             className='mwInput pl-6 w-full'
             placeholder='비밀번호'
+            {...register('password')}
           />
+          {errors.password && (
+            <p className='text-sm text-alert font-semibold pt-3'>
+              {errors.password.message}
+            </p>
+          )}
         </div>
         <div className='mt-8'>
           <label htmlFor='text'></label>
           <input
-            type='email'
+            type='text'
             className='mwInput pl-6 w-full'
             placeholder='이메일'
+            {...register('email')}
           />
+          {errors.email && (
+            <p className='text-sm text-alert font-semibold pt-3'>
+              {errors.email.message}
+            </p>
+          )}
         </div>
         <div className='mt-8'>
           <label htmlFor='mobile'></label>
@@ -43,14 +103,15 @@ const SignInPage = (props: Props) => {
             type='text'
             className='mwInput pl-6 w-full'
             placeholder='모바일'
+            {...register('mobile')}
           />
+          {errors.mobile && (
+            <p className='text-sm text-alert font-semibold pt-3'>
+              {errors.mobile.message}
+            </p>
+          )}
         </div>
-        <button
-          onClick={() => {
-            navigate('/signin/optinfo');
-          }}
-          className='mwBtn !w-[300px] font-semibold mt-12'
-        >
+        <button type='submit' className='mwBtn !w-[300px] font-semibold mt-12'>
           회원가입 하기
         </button>
       </form>
