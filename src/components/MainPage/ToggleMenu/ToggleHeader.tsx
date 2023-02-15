04819cb498301d16524e0a21ef34444cbfb33dc9
조승후 <choseunghu@joseunghuui-MacBookAir.local>
2023-02-15 21:52:05 +0900

    [#7] Feat: 메뉴토글 헤더 작업중

---
+++ src/components/MainPage/ToggleMenu/ToggleHeader.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+type props = {};
+
+const ToggleHeader = (props: props) => {
+  const [logIn, setlogIn] = useState(false);
+  const logInCheck = () => {
+    if(props){
+
+    }
+  return (
+    <div className=''>
+      <div></div>
+    </div>
+  );
+};
+
+export default ToggleHeader;
