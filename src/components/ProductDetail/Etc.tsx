460c60b8cc4f354ce0e4aa7619f0edb2fd6913dc
hyorimm <hyorimm@gmail.com>
2023-02-17 20:33:41 +0900

    [#42] Feat: 하단 내용 부분 컴포넌트 분리

---
+++ src/components/ProductDetail/Etc.tsx
@@ -0,0 +1,15 @@
+const Etc = () => {
+  return (
+    <ul className='px-[20px]'>
+      <li className='flex w-full flex-row items-center my-3'>
+        <span className='basis-1/3 font-bold text-[18px]'>기타상품정보</span>
+        <span className='basis-2/3 text-right'></span>
+      </li>
+      <li className='flex w-full flex-row items-center my-3'>
+        <span className='basis-1/3 font-bold text-[18px]'>대출 계좌 번호</span>
+        <span className='basis-2/3 text-right'>123-456-78-910</span>
+      </li>
+    </ul>
+  );
+};
+export default Etc;
