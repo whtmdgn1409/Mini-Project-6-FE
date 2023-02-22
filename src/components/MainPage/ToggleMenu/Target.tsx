312de6b75ef8e0a2e6a051301586f12045ab00cf
조승후 <choseunghu@joseunghuui-MacBookAir.local>
2023-02-22 13:40:35 +0900

    [#7] Feat: 토글 메뉴

--- src/components/MainPage/ToggleMenu/Target.tsx
+++ src/components/MainPage/ToggleMenu/Target.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { targetList } from '../../../utils/infoData';
 type props = {};
-const targetList = ['근로자', '사업가', '소상공인', '사회적경제기업', '기타'];
 const Target = (props: props) => {
   return (
     <div className='flex flex-col w-[280px] gap-3'>
       {targetList.map((list) => {
-        return <div className=''>{list}</div>;
+        return (
+          <div key={list} className=''>
+            {list}
+          </div>
+        );
       })}
     </div>
   );
