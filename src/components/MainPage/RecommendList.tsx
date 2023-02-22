df9aed0f8ba0906075441304830bc1070d48b48a
조승후 <choseunghu@joseunghuui-MacBookAir.local>
2023-02-22 13:41:50 +0900

    [#48] Feat: api 연결

--- src/components/MainPage/RecommendList.tsx
+++ src/components/MainPage/RecommendList.tsx
@@ -1,43 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Slick from '../../slider/slick';
+import {
+  getProductType,
+  memberRecommend,
+  nomemberRecommend,
+  getUserInfo,
+  ProductList,
+} from '../../api/axios';
+type props = {};
 
-interface itemsProps {
-  item: string;
-  name: string;
-}
-
-const RecommendList = () => {
-  const items: itemsProps[] = [
-    {
-      item: 'http://placehold.it/1200x400',
-      name: '이미지01',
-    },
-    {
-      item: 'http://placehold.it/1200x400/ff0000',
-      name: '이미지02',
-    },
-    {
-      item: 'http://placehold.it/1200x400/00ffff',
-      name: '이미지03',
-    },
-    {
-      item: 'http://placehold.it/1200x400/00ffff',
-      name: '이미지04',
-    },
-  ];
-  return (
+const RecommendList = (props: props) => {
+  const [memberlists, setmemberlists] = useState<Array<ProductList>>([]);
+  const [nomemberlists, setnomemberlists] = useState<Array<ProductList>>([]);
+  const [login, setlogin] = useState(Boolean);
+  useEffect(() => {
+    async function fetchMemberData() {
+      const memberrecommendList = await memberRecommend();
+      setmemberlists(memberrecommendList);
+    }
+    async function fetchNoMemberData() {
+      const nomemberrecommendList = await nomemberRecommend();
+      setnomemberlists(nomemberrecommendList);
+    }
+    async function fetchLogin() {
+      const checkLogin = await getUserInfo();
+      setlogin(checkLogin);
+    }
+    fetchMemberData();
+    fetchNoMemberData();
+    fetchLogin();
+  }, []);
+  console.log(nomemberlists);
+  //로그인이 되어 있을 때
+  return login ? (
     <div className='relative top-10 max-w-ms z-0'>
       <p className='font-semibold text-2xl p-10 ml-3 box-border'>
         <span className='text-mw'>미왕이</span> 님을 위한 추천상품이에요.
       </p>
       {/* 추천 상품 리스트 보여주기 */}
       <div className='SlickContainer w-80 h-36 ml-15 m-auto box-border'>
-        <Slick>
-          {items.map((item, index) => (
-            <div key={index} className='shadow-default'>
-              <img className='w-80 h-28' src={item.item} alt={item.name} />
+        {/* <Slick>
+          {lists.map((item) => (
+            <div key={item.snq} className='shadow-default'>
+              <div>
+                <p>{item.loanName}</p>
+                <p>{item.loanTarget}</p>
+              </div>
             </div>
           ))}
+        </Slick> */}
+      </div>
+    </div>
+  ) : (
+    //로그인이 안 되어 있을 때
+    <div className='relative top-10 max-w-ms z-0'>
+      <p className='font-semibold text-2xl p-10 ml-3 box-border'>
+        추천상품이 있어요
+      </p>
+      {/* 추천 상품 리스트 보여주기 */}
+      <div className='SlickContainer w-80 h-36 ml-15 m-auto box-border'>
+        <Slick>
+          {/* {nomemberlists.map((item) => (
+            <div key={item.snq} className='shadow-default'>
+              <div>
+                <p>{item.loanName}</p>
+                <p>{item.loanTarget}</p>
+              </div>
+            </div>
+          ))} */}
         </Slick>
       </div>
     </div>
