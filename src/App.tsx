b86e128f0b42a67af95532cb8deb752bb23617f1
quokka-eating-carrots <ovzip7@gmail.com>
2023-02-26 13:43:55 +0900

    [#25] Feat: 메뉴바 고정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { ScrollRestoration, Outlet } from 'react-router-dom';
 import Header from './components/Header';
-import MenuBar2 from './components/MenuBar2';
+import MenuBar from './components/MenuBar';
 
 function App() {
   return (
-    <div className='w-[375px] m-auto'>
+    <div className='w-[375px] m-auto mb-[150px]'>
       <ScrollRestoration />
       <Header />
       <Outlet />
-      <MenuBar2 />
+      <MenuBar />
     </div>
   );
 }
